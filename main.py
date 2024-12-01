8bb1a51f544e1e35a3c4c99e85179c4cd9124de9
Giuseppe Speranza <gsperanza204@protonmail.com>
2024-12-01 17:28:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,24 @@ from time import sleep
 from pyngrok import ngrok
 import os
 
-# Configura argparse per ottenere il token dalla riga di comando
 parser = argparse.ArgumentParser(description="Esegui ngrok con autenticazione.")
 parser.add_argument("--token", required=True, help="Il token di autenticazione di ngrok")
 args = parser.parse_args()
 
-# Imposta il token di autenticazione
 ngrok.set_auth_token(args.token)
 
-# Connetti ngrok al tuo server locale
 url = ngrok.connect(8000)
 
-# Percorso del file dove salvare l'URL
 file_path = "ngrok_url.txt"
 
-# Elimina il file se esiste, quindi crea un nuovo file con il valore di `url`
 if os.path.exists(file_path):
     os.remove(file_path)
 
 with open(file_path, "w") as file:
-    file.write(str(url))
+    string = url.read().split()[1]
+    string = string[1:len(string) - 1]
+    file.write(str(string))
 
 print("Public URL:", url)
-
-# Loop infinito per mantenere attiva la connessione
 while True:
-    sleep(50)
+    sleep(10000)
